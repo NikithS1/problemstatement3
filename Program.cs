daf24e5815d20745241ef327541d7e1bea42318f
Nikiths1 <nikith.s1@mphasis.com>
2023-07-12 14:57:52 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Example using TryParse method to convert string to int
+        string intString = "123";
+        int intValue;
+        if (int.TryParse(intString, out intValue))
+        {
+            Console.WriteLine("TryParse successful. Converted integer value: " + intValue);
+        }
+        else
+        {
+            Console.WriteLine("TryParse failed. Invalid input string.");
+        }
+
+        // Example using Convert method to convert string to double
+        string doubleString = "3.14";
+        try
+        {
+            double doubleValue = Convert.ToDouble(doubleString);
+            Console.WriteLine("Convert successful. Converted double value: " + doubleValue);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Convert failed. Invalid input format.");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Convert failed. Input value is too large or too small.");
+        }
+
+        // Example using Parse method to convert string to decimal
+        string decimalString = "10.25";
+        try
+        {
+            decimal decimalValue = decimal.Parse(decimalString);
+            Console.WriteLine("Parse successful. Converted decimal value: " + decimalValue);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Parse failed. Invalid input format.");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Parse failed. Input value is too large or too small.");
+        }
+    }
+}
